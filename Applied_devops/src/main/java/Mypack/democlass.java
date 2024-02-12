5af9aa3d93e3b90c097867ce64da94b6d327e310
Siddhijain1808 <whiskyjain30@gmail.com>
2024-02-12 17:08:07 +0530

    Add files via upload

---
+++ Applied_devops/src/main/java/Mypack/democlass.java
@@ -0,0 +1,17 @@
+package Mypack;
+
+public class democlass {
+
+	public void show() {
+		System.out.println("Hello Devops! Welcome to jenkins");
+	}
+
+	public static void main(String[] args) {
+		democlass ob=new democlass();
+		ob.show();
+	
+		
+
+	}
+
+}
